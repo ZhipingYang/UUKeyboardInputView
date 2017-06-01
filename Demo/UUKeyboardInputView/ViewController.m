7929a26710d496a2b306767e747050eb84e765b8
XcodeYang <yangzhiping@chelun.com>
2017-06-01 18:35:30 +0800

    update project config

--- Demo/UUKeyboardInputView/ViewController.m
+++ Demo/UUKeyboardInputView/ViewController.m
@@ -17,7 +17,6 @@
     NSString *content = sender.tag == 2 ? [sender titleForState:UIControlStateNormal] : @"";
     UIColor *color = sender.tag==0 ? [UIColor colorWithWhite:0 alpha:0.5]:[UIColor clearColor];
     
-    
     [UUKeyboardInputView showKeyboardConfige:^(UUInputConfiger * _Nonnull configer) {
         // 配置信息（后续可继续添加）
         configer.keyboardType = type;
@@ -28,7 +27,6 @@
         // 回调事件处理
         if (contentStr.length == 0) return ;
         [sender setTitle:contentStr forState:UIControlStateNormal];
-        
     }];
 }
 
